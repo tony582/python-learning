cc1246106b8a944f6687c8afa8e8efa0868485e3
dulaidila <dulaidila@gmail.com>
2025-11-14 15:53:17 +0800

    update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
 print("Hello from VS Code!")
 name = input("请输入你的名字：")
 print("你好，", name)
-
+print("Hello from VS Code!")
+name = input("请输入你的名字：")
+print("你好，", name)
+print("Hello from VS Code!")
+name = input("请输入你的名字：")
+print("你好，", name)
+print("Hello from VS Code!")
+name = input("请输入你的名字：")
+print("你好，", name)
